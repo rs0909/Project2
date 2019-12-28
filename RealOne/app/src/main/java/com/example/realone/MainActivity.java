f8ef227c19c767f47b92abb67af2f065d4f49b7e
rs0909 <rimsoo96@gmail.com>
2019-12-28 22:00:47 +0900

    메인 업그레이드

--- RealOne/app/src/main/java/com/example/realone/MainActivity.java
+++ RealOne/app/src/main/java/com/example/realone/MainActivity.java
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +18,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        ListView listview = (ListView)findViewById(R.id.listview);
+
+        ArrayList<String> list = new ArrayList<>();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                list
+        );
+
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+
+
     }
 }
