f74f4f94e67284f53fa145deb16d7504fa34583b
loin3 <ya6675@naver.com>
2019-12-30 20:54:30 +0900

    pract

--- RealOne/app/src/main/java/com/example/realone/MainActivity.java
+++ RealOne/app/src/main/java/com/example/realone/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         callPermission();
-        
+
     }
 
     private void callPermission() {
