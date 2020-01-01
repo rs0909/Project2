5be0871dbe7df1e1dbbb9a2d55f8c4bf8b427b4d
loin3 <ya6675@naver.com>
2020-01-01 23:01:30 +0900

    as you wish

--- RealOne3/app/src/main/java/com/example/realone/LargePhotoDialog.java
+++ RealOne3/app/src/main/java/com/example/realone/LargePhotoDialog.java
@@ -72,12 +72,54 @@ public class LargePhotoDialog {
         });
     }
 
+    public void callFunction(Uri photoUri, final String phoneNumber, String selectedOne){
+        final Dialog dialog = new Dialog(context);
+        dialog.setContentView(R.layout.dialog_large_photo);
+
+        WindowManager.LayoutParams wm = new WindowManager.LayoutParams();
+        wm.copyFrom(dialog.getWindow().getAttributes());
+        wm.width = 1000;
+        wm.height = 1000;
+        dialog.show();
+        Window window = dialog.getWindow();
+        window.setAttributes(wm);
+
+        PhotoView photoView = (PhotoView) dialog.findViewById(R.id.photoView);
+        Button contactButton = (Button)dialog.findViewById(R.id.button);
+        Button cancelButton = (Button)dialog.findViewById(R.id.button2);
+        TextView textView = (TextView)dialog.findViewById(R.id.textView);
+
+        photoView.setImageURI(photoUri);
+        int count = getHowManyTimesCallWith(selectedOne);
+        if(count != -1){
+            textView.setText("" + count);
+        }
+
+        contactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + phoneNumber.replaceAll("-", "")));
+                context.startActivity(intent);
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+    }
+
     public int getHowManyTimesCallWith(String phoneNumber){
+        Log.d("diqkf1", phoneNumber);
         SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase(dbname, Context.MODE_PRIVATE, null);
         Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM CONTACT_HISTORY_TABLE", null);
         if(cursor.moveToFirst()){
             do{
-                if(phoneNumber.equals(cursor.getString(cursor.getColumnIndex("PHONE")))){
+                if(phoneNumber.replaceAll("-", "").equals(cursor.getString(cursor.getColumnIndex("PHONE")))){
                     return cursor.getInt(cursor.getColumnIndex("COUNT"));
                 }
             }while (cursor.moveToNext());
